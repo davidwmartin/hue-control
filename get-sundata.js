47bf2a7135e13a87b0e9785b5b81db334e7a40b9
David Martin <david.w.martin@me.com>
2019-12-08 14:47:50 -0800

    can get appropriate scene based on time of day

---
+++ get-sundata.js
@@ -0,0 +1,32 @@
+var http = require('http'),
+		config = require('./config/config.js');
+
+
+////// Make call to sunrise-sunset API to get sun timing info for today
+function getSunData() { 
+
+	// request vars
+	var options = {
+		// params for lattitude + longitude, date, formatting
+		host: 'http://api.sunrise-sunset.org/json?lat='+config.lattitude+'&lng='+config.longitude+'&date=today&formatted=0'
+	}
+
+	return new Promise(function(resolve, reject) {
+
+		// executes http request to get sunset time
+		http.get(options.host, function(res){
+			var data = "";
+		  
+		  res.setEncoding('utf8');
+		  res.on('data', function (chunk) {
+		  	var sunData = JSON.parse(chunk);
+		  	resolve(sunData.results);
+		  });
+		});
+
+
+	});
+
+}
+
+module.exports = getSunData;
--- lights.js
+++ lights.js
@@ -2,7 +2,9 @@
 var fs = require('fs'), 
 		hue = require('node-hue-api'),
 		config = require('./config/config.js'),
-		connect = require('./hue-connect.js')
+		connect = require('./hue-connect.js'),
+		getSunData = require('./get-sundata'),
+		sceneTimes = require('./config/scene-times.js')
 		;
 
 // define some vars
@@ -28,13 +30,13 @@ connect().then(function(data){
 
 
 // parses command, changes lights
-function changeLights(commandType, api){
+async function changeLights(commandType, api){
 
 	var sceneId;
 
 	switch (commandType){
 		case 'on':
-			sceneId = '44vKzz9DvQNo46l';
+			sceneId = await getSceneByTime();
 			break;
 
 		case 'off':
@@ -89,6 +91,28 @@ function changeLights(commandType, api){
 	api.activateScene(sceneId)
 		.then(displayResult(commandType))
 		.done();
+}
+
+// gets sunset time, compares against current time and selects a scene from config object based on offsets defined therein
+async function getSceneByTime() {
+	var now = new Date();
+
+	var sunset;
+	var desiredSceneId;
+
+	await getSunData().then(function(result){
+		sunset = new Date(result.sunset);
+	});
+
+	var sunsetOffset = (now - sunset) / 1000 / 60; // convert to minutes
+	// console.log(sceneTimes);
+	for (var i = sceneTimes.length - 1; i >= 0; i--) {
+		if (sceneTimes[i].offset < sunsetOffset) {
+			desiredSceneId = sceneTimes[i].scene;
+			break;
+		}
+	}
+	return desiredSceneId;
 }
 
 
